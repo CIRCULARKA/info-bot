ed496bde6425a462614fe47532ca629a8611ed5d
4view <akochergin.2002@gmail.com>
2024-12-09 13:34:49 +0500

    feat: Add EF Core

---
+++ src/InfoBot.Data/Usings.cs
@@ -0,0 +1,8 @@
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+global using InfoBot.Core.Entities;
+global using InfoBot.Core.Models;
+global using InfoBot.Data.Configurations;
