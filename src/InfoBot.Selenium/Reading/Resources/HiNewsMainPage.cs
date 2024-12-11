ff5d9f49cf40df4bb50070023589acc41d755078
CIRCULARKA <donkardon@list.ru>
2024-12-11 20:01:25 +0500

    feat: Make basic implementation of article reading

--- src/InfoBot.Selenium/Reading/Resources/HiNewsMainPage.cs
+++ src/InfoBot.Selenium/Reading/Resources/HiNewsMainPage.cs
@@ -2,13 +2,51 @@ namespace InfoBot.Selenium.Reading.Resources;
 
 public class HiNewsMainPage : Page, IResourceMainPage
 {
-    public HiNewsMainPage(IWebDriver driver, TimeSpan explicitWaitTime) :
-        base(driver, explicitWaitTime)
+    /// <summary>
+    /// Путь до корневого веб-элемента каждой статьи на странице
+    /// </summary>
+    private readonly By _articleSelector = By.XPath("//article");
+
+    /// <summary>
+    /// Путь до заголовка статьи относительно веб-элемента статьи
+    /// </summary>
+    private readonly By _articleHeaderSelector = By.XPath("h2/a");
+
+    public HiNewsMainPage(
+        Uri pageUri,
+        IWebDriver driver,
+        TimeSpan explicitWaitTime,
+        TimeSpan pageLoadTimeout) :
+        base(pageUri, driver, explicitWaitTime, pageLoadTimeout)
     {
     }
 
     public List<Article> ReadLatestArticles()
     {
-        throw new NotImplementedException();
+        Load();
+
+        var articleElements = GetElements(_articleSelector);
+
+        var result = new List<Article>();
+        foreach (var articleElement in articleElements)
+        {
+            var headerAttribute = GetHeaderAttribute(articleElement);
+            result.Add(new Article
+            {
+                Attributes = new() { headerAttribute }
+            });
+        }
+
+        return result;
+    }
+
+    private ArticleAttribute GetHeaderAttribute(IWebElement article)
+    {
+        var header = article.FindElement(_articleHeaderSelector).Text.Trim();
+        return new ArticleAttribute
+        {
+            Name = "Header",
+            Value = header
+        };
     }
 }
