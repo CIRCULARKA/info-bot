90301fe5b0505ced185ff4a2501963de269caa03
CIRCULARKA <donkardon@list.ru>
2024-12-11 19:59:57 +0500

    build: Add test project

---
+++ src/InfoBot.Tests/Usings.cs
@@ -0,0 +1,11 @@
+global using Xunit;
+global using Xunit.Abstractions;
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Chrome;
+
+global using System.Text;
+
+global using InfoBot.Selenium;
+global using InfoBot.Selenium.Reading;
+global using InfoBot.Selenium.Reading.Resources;
+global using InfoBot.Tests.Integration;
